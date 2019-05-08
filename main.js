4a9cf9199cf5ab0f6a4931b9c9597f4ed6fa4aee
qqhann <qiu.gits@gmail.com>
2019-05-09 02:58:30 +0900

    Init

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600,
+    })
+    win.loadFile('index.html')
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win == null) {
+        createWindow()
+    }
+})
+
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
