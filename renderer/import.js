1c35c4347d5c453145de0475e235eee5aca949d1
qqhann <qiu.gits@gmail.com>
2019-06-02 15:52:54 +0900

    Refactor: login

--- renderer/import.js
+++ renderer/import.js
@@ -1,5 +1,4 @@
 const links = document.querySelectorAll('link[rel="import"]')
-console.log('import js')
 
 // Import and add each page to the DOM
 // Thanks: https://github.com/electron/electron-api-demos/blob/2a8c1f46b582b2762c2cf0afc7559082726255b5/assets/imports.js
--- renderer/login.js
+++ renderer/login.js
@@ -7,30 +7,16 @@ const appName = app.getName() // will deprecate. use app.name in the future
 const usernameInput = document.getElementById('username')
 const passwordInput = document.getElementById('password')
 const loginBtn = document.getElementById('login-btn')
-let username = null
-let password = null
 
 // Store input data
-const getUsername = (usernameTarget) => {
-    return usernameTarget.value
-}
-const getPassword = (passwordTarget) => {
-    return passwordTarget.value
-}
-
-usernameInput.addEventListener('input', (e) => {
-    username = getUsername(e.target)
-    store.set('username', username)
-})
-passwordInput.addEventListener('input', (e) => {
-    // password = getPassword(e.target)
-})
 loginBtn.addEventListener('click', (e) => {
-    username = getUsername(usernameInput)
-    password = getPassword(passwordInput)
+    const username = usernameInput.value
+    const password = passwordInput.value
+    store.set('username', username)
     keytar.setPassword(appName, username, password)
+
+    passwordInput.value = null
 })
 
 // Load saved data (on DOM load?)
 usernameInput.value = store.get('username')
-passwordInput.value = store.get('password')
