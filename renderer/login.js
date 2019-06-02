8dfb4587739260e693ddf1956de049427217217b
qqhann <qiu.gits@gmail.com>
2019-06-02 14:48:13 +0900

    Update: login with btn, not by input

--- renderer/login.js
+++ renderer/login.js
@@ -1,15 +1,34 @@
+const { app } = require('electron').remote
+const keytar = require('keytar')
 const Store = require('electron-store')
 const store = new Store()
 
+const appName = app.getName() // will deprecate. use app.name in the future
 const usernameInput = document.getElementById('username')
 const passwordInput = document.getElementById('password')
+const loginBtn = document.getElementById('login-btn')
+let username = null
+let password = null
 
 // Store input data
+const getUsername = (usernameTarget) => {
+    return usernameTarget.value
+}
+const getPassword = (passwordTarget) => {
+    return passwordTarget.value
+}
+
 usernameInput.addEventListener('input', (e) => {
-    store.set('username', e.target.value)
+    username = getUsername(e.target)
+    store.set('username', username)
 })
 passwordInput.addEventListener('input', (e) => {
-    store.set('password', e.target.value)
+    // password = getPassword(e.target)
+})
+loginBtn.addEventListener('click', (e) => {
+    username = getUsername(usernameInput)
+    password = getPassword(passwordInput)
+    keytar.setPassword(appName, username, password)
 })
 
 // Load saved data (on DOM load?)
